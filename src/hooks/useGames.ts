9cf9bdf4dc8b09887371fdb213f6e2a9827d72c7
syednomansd <syednomansd@gmail.com>
2023-06-15 18:56:33 +0500

    filter games by genre

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,19 +1,23 @@
-import useData from './useData';
+import useData from "./useData";
+import { Genres } from "./useGenres";
 
 export interface Platform {
-	id: number;
-	name: string;
-	slug: string;
+  id: number;
+  name: string;
+  slug: string;
 }
 
 export interface Game {
   id: number;
   name: string;
-	background_image: string;
-	parent_platforms: { platform: Platform }[];
-	metacritic: number;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
 
-const useGames = () => useData<Game>('/games');
+const useGames = (selectedGenre: Genres | null) =>
+  useData<Game>("/games", { params: { genres: selectedGenre?.id } }, [
+    selectedGenre?.id,
+  ]);
 
 export default useGames;
